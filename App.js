bf31866d6c90874cb5f3083b09a3c16e6ed4a574
hinodi <huynhngocdinh98@gmail.com>
2023-07-17 13:49:59 +0700

    test React.lazy with Expo SDK 49

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import { lazy, Suspense, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Detail = lazy(() => import('./Detail'));
 
 export default function App() {
+  const [showDetail, setShowDetail] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Show Detail" onPress={() => setShowDetail(true)} />
+      {showDetail && (
+        <Suspense fallback={<View />}>
+          <Detail />
+        </Suspense>
+      )}
     </View>
   );
 }
